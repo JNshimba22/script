7df50550096cc32697d37844f4f81ba5db6f523d
JNshimba22 <joelleilda@gmail.com>
2022-06-07 22:06:55 -0400

    adding a bash

--- file2.sh
+++ file2.sh
@@ -1,4 +1,20 @@
+#!/bin/bash
 # This is my first time to use git
 
 echo "hello world"
 echo "Another code"
+echo "I am learninh how to use git and github"
+ 1.mkdir forlder
+ 2.cd folder
+ 3.git status
+ 4.git init
+ 5.code fileName
+ 6.git status
+ 7. git add fileName or .
+ 8. git commit -m "changes note"
+ 9. git status
+ 10. git log
+11. git branch
+12. git branch Name1
+13. git branch
+14. git checkout Name1
